afacb3f0c9bb6a1e1d332b49ad8a5dc11e777579
xiongjinxin <222042015@link.cuhk.edu.cn>
2024-04-26 15:49:15 +0800

    add datasets for nonconvex qp

--- datasets/dcopf/make_dataset_nonconvexqp.py
+++ datasets/dcopf/make_dataset_nonconvexqp.py
@@ -1,6 +1,5 @@
 import osqp
 import time
-import tqdm
 import random
 import pickle
 import numpy as np
@@ -12,13 +11,13 @@ import sys
 import os
 import scipy.io as spio
 
-
 sys.path.insert(1, os.path.join(sys.path[0], os.pardir, os.pardir))
 
 from dcopf_utils import build_gurobi_model
-filepath = "/home/jxxiong/A-xjx/DC3/datasets/dcopf/qp20_10_1_1.mat"
-# with open(filepath, 'rb') as f:
-#     data = pickle.load(f)
+
+
+filepath = "/home/jxxiong/A-xjx/DC3/datasets/dcopf/qp50_25_4_4.mat"
+
 data = spio.loadmat(filepath)
 b = data['beq'].squeeze()
 # ranfomly generate X as numsamples of random perturbation of b by factors randomly generated from [0.9, 1.1]
@@ -33,19 +32,20 @@ Ub = data['UB'].squeeze()
 
 X = []
 while len(X) < num_samples:
-    x = np.random.rand(b.shape[0]) * 0.2 + 0.9
+    x = np.random.rand(b.shape[0]) * 0.6 + 0.7
     x = x * b
     full_model = build_gurobi_model(Q, p, A, x, G, h, Lb, Ub)
     full_model.setParam('OutputFlag', 1)
+    full_model.setParam('TimeLimit', 10)
     # full_model.setParam('Presolve', -1)
     full_model.optimize()
-    if full_model.status == GRB.OPTIMAL:
+    if full_model.status == GRB.OPTIMAL and full_model.Runtime > 1:
         X.append(x)
-
+    print(f"X: {len(X)}")
 X = np.array(X)
 print(X.shape)
 
 dataset = {'Q':data['H'], 'p':data['f'].squeeze(), 'A':data['Aeq'], 'X':X, 'G':data['A'], 'h':data['b'].squeeze(), 'Lb':data['LB'].squeeze(), 'Ub':data['UB'].squeeze(), 'Y':[]}
-with open(f"dcopf20_data", 'wb') as f:
+with open(f"dcopf51_data", 'wb') as f:
     pickle.dump(dataset, f)
         
